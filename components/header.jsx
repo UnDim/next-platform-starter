7345cc2bd9a2a1a2d52e5ff64ebec4ebe42ef04c
undim <shannonodam@icloud.com>
2024-11-01 23:02:47 -0400

    Updated the header

--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import netlifyLogo from 'public/netlify-logo.svg';
+import jeepLogo from 'public/logo.svg';
 import githubLogo from 'public/images/github-mark-white.svg';
 
 const navItems = [
@@ -16,7 +16,7 @@ export function Header() {
     return (
         <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
             <Link href="/">
-                <Image src={netlifyLogo} alt="Netlify logo" />
+                <Image src={jeepLogo} alt="Netlify logo" />
             </Link>
             {!!navItems?.length && (
                 <ul className="flex flex-wrap gap-x-4 gap-y-1">
@@ -38,7 +38,7 @@ export function Header() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <Image src={githubLogo} alt="GitHub logo" className="w-7" />
+                    {/* <Image src={githubLogo} alt="GitHub logo" className="w-7" /> */}
                 </Link>
             </div>
         </nav>
